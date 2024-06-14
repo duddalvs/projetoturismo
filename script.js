4529fcd85280affe6143c06f032e738d3c01dd70
Maria Eduarda Alves <duda.alves.machado@gmail.com>
2024-06-14 17:17:16 -0300

    projeto da faculdade

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-function validarForm() {
-    var senha = document.getElementById('senha').value;
-    var repetirSenha = document.getElementById('repetirSenha').value;
-    if (senha !== repetirSenha) {
-      document.getElementById('senhaMismatch').classList.remove('hidden');
-      document.getElementById('senha').value = ''; // Limpa o campo senha
-      document.getElementById('repetirSenha').value = ''; // Limpa o campo repetirSenha
-      return false;
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagens/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagens/close_white_36dp.svg";
     }
-    return true;
-  }
-  
-  function fecharAviso() {
-    document.getElementById('senhaMismatch').classList.add('hidden');
-    document.getElementById('senha').value = ''; // Limpa o campo senha
-    document.getElementById('repetirSenha').value = ''; // Limpa o campo repetirSenha
-  }
-  
+}
+
+
+function initMap() {
+    // A localização de exemplo (Nova Iorque)
+    var location = {lat: -22.866688, lng: -43.253763};
+    // O mapa, centralizado na localização
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: location
+    });
+    // O marcador, posicionado na localização
+    var marker = new google.maps.Marker({position: location, map: map});
+}
+
